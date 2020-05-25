858510c8986938d3f26533570d0e5474dd142033
Umidjon <webdevolopers01@gmail.com>
2020-05-25 12:58:52 +0300

    Bu oxirgi yangilash

--- routes/directors.js
+++ routes/directors.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const Category = require('../model/category');
 
@@ -18,40 +19,94 @@ router.post('/api/directors', (req, res, next) => {
 });
 
 router.get('/api/directors', (req, res, next) => {
-    const promise = Category.aggregate([
+    const directory = Category.aggregate([
         {
             $lookup: {
                 from: 'movies',
                 localField: '_id',
                 foreignField: 'director_id',
-                as: 'Kinolari: '
+                as: 'Kinolari:'
             }
         },
-        // {
-        //     $unwind: {
-        //         path: '$movies'
-        //     }
-        // },
         {
-            $group:{
-                _id:{
-                    _id: '$_id',
+            $unwind: {
+                path: '$movies'
+            }
+        },
+        {
+            $group: {
+                _id: {
                     name: '$name',
-                    surname: '$surname',
-                    bio: '$bio',
-                    movies: '$movies'
+                    surname: '$surname'
                 }
             }
         }
     ]);
+       
 
-    promise
+    directory
     .then((data) => {
         res.send(data);
     })
     .catch((err) => {
         res.send(err);
+    });
+});
+
+
+router.get('/api/directors/:id', (req, res, next) => {
+    const id = req.params.id;
+    const category = Category.aggregate([
+        {
+            $match: {
+                _id: mongoose.Types.ObjectId(id)
+            }
+        },
+        {
+            $lookup:{
+                from: 'movies',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'Kinolari: '
+            }
+        }
+    ]);
+
+    category
+    .then((data) => {
+        res.send(data);
     })
+    .catch((err) => {
+        res.send(err);
+    });
+})
+
+router.put('/api/directors/:id', (req, res, next) => {
+    const id = req.params.id;
+    const directory = Category.findByIdAndUpdate(
+        id,
+        req.body
+    );
+    directory
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.send(err);
+        });
+});
+
+router.delete('/api/directors/:id', (req, res, next) => {
+    const id = req.params.id;
+    const del = Category.findByIdAndDelete(id);
+
+    del
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.send(err);
+        })
 });
 
 
